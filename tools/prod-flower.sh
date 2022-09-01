5082c39f0e00b2aeb45ef4b1d11193a0f6ef445c
Jingli Shi <jingli.shi@aut.ac.nz>
2022-09-01 14:43:35 +1200

    Update prod-flower.sh
    delete flower

--- tools/prod-flower.sh
+++ tools/prod-flower.sh
@@ -9,8 +9,6 @@ cd /backend
   echo "Waiting for database"
   python manage.py wait_for_db
 
-  echo "Starting flower"
-  if [[ -n "${FLOWER_BASIC_AUTH}" ]]; then
-    celery --app=config flower --basic_auth="${FLOWER_BASIC_AUTH}"
-  fi
+
+
 )
